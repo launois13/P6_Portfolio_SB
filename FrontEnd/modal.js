fd86114d2fe5240bd62dd9492d422a4ba3256da4
launois13 <pascalelaunois@free.fr>
2024-06-19 20:43:11 +0200

    etape 1.2

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -4,10 +4,13 @@ function openModal(){
     const modal = document.getElementById('modal');
 
     buttonOpen.addEventListener('click', () => {
+        document.body.style.overflowY = "hidden"; // retire les barres de scrolls
         modal.classList.add('open-modal');
+        window.scrollTo(0, 0); //scroll en haut de la page
     });
 
     buttonClose.addEventListener('click', () => {
+        document.body.style.overflowY = "scroll"; // remet les barres de scrolls
         modal.classList.remove('open-modal');
     });
 }
